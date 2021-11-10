79cb0601231485a6a9c166cbf1d41f90407e932b
AlexS2 <rb-reklama@yamdex.ru>
2021-11-10 16:51:24 +0500

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FILESORT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
